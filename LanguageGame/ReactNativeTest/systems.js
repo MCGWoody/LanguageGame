335391d8d33dd3bd0634abb4e4c7c51104657878
Your Name <you@example.com>
2020-07-31 10:34:21 -0700

    for each error'

--- LanguageGame/ReactNativeTest/systems.js
+++ LanguageGame/ReactNativeTest/systems.js
@@ -1,13 +1,36 @@
-const MoveWordBlocks = (entities, { touches }) => {
 
-  touches.filter(t => t.type === "move").forEach(t => {
-    let finger = entities[t.id];
-    if (finger && finger.position) {
-      finger.position = [
-        t.newPageX,
-        t.newPageY
+selectedEnt = null;
+
+const MoveWordBlocks = (entities, { touches }) => 
+{
+
+  touches.filter(t => t.type === "start").forEach(t => {
+    // check select entities
+    var gridX = t.newPageX / 100;
+    var gridY = (t.newPageY - 50) / 100;
+
+    var didSelectEnt = false;
+
+    entities.forEach(ent => {
+      var gridEntX = ent.position[0] / 100;
+      var gridEntY = (ent.position[1] - 50) / 100;
+      if( gridEntX == gridX && gridEntY == gridY )
+      {
+        didSelectEnt = true;
+        selectedEnt = ent;
+      }
+    });
+
+    //check for a move
+    if( !didSelectEnt && selectedEnt )
+    {
+      selectedEnt.position[
+        gridX * 100,
+        (gridY * 100) + 50
       ];
     }
+
+
   });
   
   return entities;
